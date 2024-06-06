9da567cfff0a6f11fb25cc5d713a18f04e9becc8
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-06-06 23:27:44 +0530

    error page updated and some styling

--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,5 +1,17 @@
-function Error() {
-  return <div>Error</div>;
-}
+import NavBar from "../../Components/Navbar/NavBar";
+import "../../Styles/error.scss";
+import { Link } from "react-router-dom";
 
-export default Error;
+export default function Error() {
+  return (
+    <>
+      <NavBar />
+      <div className="error">
+        <p>Something went wrong!!</p>
+        <Link to="/">
+          Go to Home <i class="fa-solid fa-house"></i>
+        </Link>
+      </div>
+    </>
+  );
+}
